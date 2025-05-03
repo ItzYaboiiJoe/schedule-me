e429bbd857472992f9b65253bb7f38d92c6d5e5a
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-05-03 12:16:20 -0400

    Completed the navigation links

--- src/components/shared/adminSideBar/index.tsx
+++ src/components/shared/adminSideBar/index.tsx
@@ -1,4 +1,11 @@
 import Link from "next/link";
+import {
+  Binoculars,
+  CalendarDays,
+  Database,
+  Clipboard,
+  Settings,
+} from "lucide-react";
 
 const AdminSideNavbar = () => {
   return (
@@ -7,14 +14,41 @@ const AdminSideNavbar = () => {
         {/* Business Name Section */}
         <div className="mb-10 text-white flex items-center flex-col">
           <div className="w-16 h-16 bg-gray-200 rounded-full mb-2"></div>
-          <span className="text-lg font-semibold">Test</span>
+          <span className="text-lg font-semibold">Business Name</span>
         </div>
 
         {/* Navigation NavLinks */}
         <ul className="w-full space-y-6">
-          <Link href={"/admin-overview"} className="buttonStyle">
-            <span className="mainStyle">Overview</span>
-          </Link>
+          <li className="w-full shadow-lg">
+            <Link href={"/admin-overview"} className="buttonStyle">
+              <Binoculars size={18} className="iconStyle mr-3" />
+              <span className="mainStyle">Overview</span>
+            </Link>
+          </li>
+          <li className="w-full shadow-lg">
+            <Link href={"/admin-calendar"} className="buttonStyle">
+              <CalendarDays size={18} className="iconStyle mr-3" />
+              <span className="mainStyle">Calendar</span>
+            </Link>
+          </li>
+          <li className="w-full shadow-lg">
+            <Link href={"/admin-services"} className="buttonStyle">
+              <Database size={18} className="iconStyle mr-3" />
+              <span className="mainStyle">Services</span>
+            </Link>
+          </li>
+          <li className="w-full shadow-lg">
+            <Link href={"/admin-reports"} className="buttonStyle">
+              <Clipboard size={18} className="iconStyle mr-3" />
+              <span className="mainStyle">Reports</span>
+            </Link>
+          </li>
+          <li className="w-full shadow-lg">
+            <Link href={"/admin-settings"} className="buttonStyle">
+              <Settings size={18} className="iconStyle mr-3" />
+              <span className="mainStyle">Settings</span>
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
