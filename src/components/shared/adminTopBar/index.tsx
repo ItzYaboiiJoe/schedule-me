5c06ed6bdf53456ba76cf5b3bb95a77a4570f15d
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-05-03 16:00:22 -0400

    Installed button from shadcn and modified add appointment to use that button

--- src/components/shared/adminTopBar/index.tsx
+++ src/components/shared/adminTopBar/index.tsx
@@ -1,12 +1,13 @@
 import { CalendarDays } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const AdminTitleBar = ({ pageTitle }: { pageTitle: string }) => {
   return (
     <header className="w-full bg-primary h-20 flex items-center justify-between px-6 shadow-lg">
       <h1 className="text-black text-xl font-medium">{pageTitle}</h1>
-      <button className="flex items-center bg-[#E9C46A] text-black px-4 py-2 rounded-lg hover:bg-[#F4A261] shadow-lg hover:shadow-2xl">
+      <Button size={"lg"} variant={"appointment"}>
         <CalendarDays size={18} className=" mr-2" /> New Appointment
-      </button>
+      </Button>
     </header>
   );
 };
