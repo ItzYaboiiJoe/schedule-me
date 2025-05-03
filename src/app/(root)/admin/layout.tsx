3c328e818fa281f08c3a4e0626e2e20c2fb5967e
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2025-05-03 13:10:59 -0400

    Modified the admin layout to use hooks and dynamically show the page title

--- src/app/(root)/admin/layout.tsx
+++ src/app/(root)/admin/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { usePathname } from "next/navigation";
 import AdminSideNavbar from "@/components/shared/adminSideBar";
 import AdminTitleBar from "@/components/shared/adminTopBar";
 
@@ -6,12 +8,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
+  const pathSegments = pathname.split("/").filter(Boolean);
+  const currentPage = pathSegments[pathSegments.length - 1];
+
+  const pageTitleMap: Record<string, string> = {
+    overview: "Overview",
+    calendar: "Calendar",
+    services: "Services",
+    reports: "Reports",
+    settings: "Settings",
+  };
+
+  const pageTitle = pageTitleMap[currentPage] || "Admin";
+
   return (
     <div className="bg-[#EAF4F1]">
       <div className="flex h-screen">
         <AdminSideNavbar />
         <div className="flex-1 flex flex-col">
-          <AdminTitleBar pageTitle="Admin" />
+          <AdminTitleBar pageTitle={pageTitle} />
           <div className="p-6">
             <h1 className="text-gray-700 text-xl font-bold">{children}</h1>
           </div>
